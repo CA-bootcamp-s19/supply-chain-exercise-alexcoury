16648530c366aae60cc9e57b483cd3bc7273741a
Alex Coury <alexcoury@MacBook-Pro.localdomain>
2019-06-18 18:36:30 -0400

    Fixed TestSupplyChain

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -5,64 +5,59 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
-    uint public intialBalance = 1 ether;
-    SupplyChain supply_chain = SupplyChain(DeployedAddresses.SupplyChain());
+    uint public initialBalance = 1 ether;
+    SupplyChain supply = SupplyChain(DeployedAddresses.SupplyChain());
 
     function beforeAll() public {
-        bool itemAdded = supply_chain.addItem("item 1", 1000);
-        Assert.equal(itemAdded, true, "Item 1 not added");
-
-        itemAdded = supply_chain.addItem("item 2", 1000);
-        Assert.equal(itemAdded, true, "Item 2 not added");
+        // add  2 items
+        // sku 0
+        bool itemAdded = supply.addItem("book 1", 1000);
+        Assert.equal(itemAdded, true, "Item not added");
+        // sku 1
+        itemAdded = supply.addItem("book 2", 1000);
+        Assert.equal(itemAdded, true, "Item not added");
     }
-
-
-    // buyItem
-
-    // test for failure if user does not send enough funds
-    // test for purchasing an item that is not for Sale
 
     function testbuyItem() public {
-        bool result;
-        (result, ) = address(supply_chain).call.value(500)(abi.encodePacked(supply_chain.buyItem.selector, uint(0)));
-        Assert.isFalse(result, "failure, insufficient enough ether");
+        bool r;
+        // test for failure if user does not send enough funds
+        (r, ) = address(supply).call.value(500)(abi.encodePacked(supply.buyItem.selector, uint(0)));
+        Assert.isFalse(r, "Should fail b/c not enought ether sent");
 
-        supply_chain.buyItem.value(5000)(0);
-        (, , , uint state, , ) = supply_chain.fetchItem(0);
-        Assert.equal(state, 1, "Not sold");
+        // test for purchasing an item that is not for Sale
+            // 1. buy the item
+        supply.buyItem.value(5000)(0);
+        (, , , uint state, , ) = supply.fetchItem(0);
+        Assert.equal(state, 1, "Item not sold.");
 
-        (result, ) = address(supply_chain).call.value(5000)(abi.encodePacked(supply_chain.buyItem.selector, uint(0)));
-        Assert.isFalse(result, "Already sold");
+            // 2. try to buy the item again. It should now be sold.
+        (r, ) = address(supply).call.value(5000)(abi.encodePacked(supply.buyItem.selector, uint(0)));
+        Assert.isFalse(r, "Item is already sold");
     }
-
-    // shipItem
-
-    // test for calls that are made by not the seller
-    // test for trying to ship an item that is not marked Sold
 
     function testshipItem() public {
-        bool result;
-        (result, ) = address(supply_chain).call(abi.encodePacked(supply_chain.shipItem.selector, uint(0)));
-        Assert.isTrue(result, "seller must be caller");
-        (result, ) = address(supply_chain).call(abi.encodePacked(supply_chain.shipItem.selector, uint(1)));
-        Assert.isFalse(result, "item is marked sold");
+        bool r;
+        // test for calls that are made by not the seller
+        (r, ) = address(supply).call(abi.encodePacked(supply.shipItem.selector, uint(0)));
+        Assert.isTrue(r, "seller is not caller");
+        // test for trying to ship an item that is not marked Sold
+        (r, ) = address(supply).call(abi.encodePacked(supply.shipItem.selector, uint(1)));
+        Assert.isFalse(r, "item marked sold");
     }
 
-    // receiveItem
-
-    // test calling the function from an address that is not the buyer
-    // test calling the function on an item not marked Shipped
-
     function testreceiveItem() public {
-        bool result;
-        (, , , uint state, , ) = supply_chain.fetchItem(0);
-        Assert.equal(state, 2, "not shipped.");
-        (result, ) = address(supply_chain).call(abi.encodePacked(supply_chain.receiveItem.selector, uint(0)));
-        Assert.isTrue(result, "buyer must be caller");
-        (result, ) = address(supply_chain).call(abi.encodePacked(supply_chain.receiveItem.selector, uint(1)));
-        Assert.isFalse(result, "not shipped");
+        bool r;
+        // ship item 0
+        //supply.shipItem(0);
+        (, , , uint state, , ) = supply.fetchItem(0);
+        Assert.equal(state, 2, "Item not shipped.");
+        // test calling the function from an address that is not the buyer
+        (r, ) = address(supply).call(abi.encodePacked(supply.receiveItem.selector, uint(0)));
+        Assert.isTrue(r, "caller is not the buyer");
+        // test calling the function on an item not marked Shipped
+        (r, ) = address(supply).call(abi.encodePacked(supply.receiveItem.selector, uint(1)));
+        Assert.isFalse(r, "item not shipped");
     }
 
     function() external payable {}
-
 }
